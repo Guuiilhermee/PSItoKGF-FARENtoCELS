487ed55ab40a64fda12caf4813995c7ea4d5ea70
Guilherme <guiilherme.ambrosio@gmail.com>
2025-09-25 16:04:52 -0300

    versão um

---
+++ prog9.js
@@ -0,0 +1,29 @@
+// PSI para KGF
+
+let resposta = document.getElementById('resposta')
+
+function principal(){
+    let psi = Number(document.getElementById('psi').value)
+    let kgf = 703.07
+
+    let calculo = (psi * kgf)
+
+    console.log(`A conversão de PSI para KFG é: ${calculo.toFixed(2)}`)
+
+    resposta.innerHTML = ``
+    resposta.innerHTML += `A conversão de PSI para KGF é: ${calculo.toFixed(2)}`
+}
+
+// °F = (°C × 1,8) + 32
+
+let resul = document.getElementById('resul')
+
+function basic(){
+
+    let faren = Number(document.getElementById('faren').value)
+    let celsi = faren * 1.8 + 32
+
+    resul.innerHTML = ``
+    resul.innerHTML += `A conversão de Fahrenheit para Celsius é: ${celsi.toFixed(2)}ºC`
+
+}
